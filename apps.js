3d8c86300fd87ff78c26f424202060f6cf83e0ea
m.dinesh kumar <156180304+diyadk1@users.noreply.github.com>
2024-03-14 17:29:02 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,8 @@
+//author dinesh kumar
+document.querySelectorAll('.button').forEach(button => button.addEventListener('click', e => {
+    if(!button.classList.contains('delete')) {
+    button.classList.add('delete');
+    setTimeout(() => button.classList.remove('delete'), 3200);
+    }
+    e.preventDefault();
+    }));
